f062dbdb991c940fcc0d087099db2e823861bfc9
Chaitanya-TP <137798479+Chaitanya-TP@users.noreply.github.com>
2023-07-21 17:19:15 +0530

    Updated Test data sheet - 2

--- src/test/java/com/koo/registrationAndLoginFlowTests/LoginKooTest.java
+++ src/test/java/com/koo/registrationAndLoginFlowTests/LoginKooTest.java
@@ -10,20 +10,26 @@ import com.framework.listeners.*;
 
 //@Listeners({SuiteEvent.class, MobileEvent.class})
 public class LoginKooTest extends BaseTest {
-    @Test(description = "[TC_006] - Verify user is able to login with phone number",
-            groups = {"regression", "registration_and_login"})
-    public void TC_006_verifyUserLoginWithOTP() throws Exception {
-        
-    	try{
-    		BaseTest.LOGGER.get().logTestStep(extentTest.get(), "INFO", "<b><----Test case execution started for Test: TC_006 - Verify user is able to login with phone number ------><b>", false, BaseTest.mobileDriver.get());
-			
-    		LoginKooPage loginKooPage = new LoginKooPage();
-    	    loginKooPage.loginValidUser("English",TestConfig.getInstance().getUserName());
-    	    BaseTest.LOGGER.get().logTestStep(extentTest.get(), "INFO", "<b><----Test case execution completed for Test:Successfully executed - TC_006 - Verify user is able to login with phone number ------><b>", true, BaseTest.mobileDriver.get());
-    	    BaseTest.afterMethodDriver.set(BaseTest.mobileDriver.get());
-    	}catch(Exception e) {
-			BaseTest.utilObj.get().getAssertManager().sAssertException("Something went wrong in verifyUserLoginWithOTP. Exception:"+e.getMessage(), true, BaseTest.mobileDriver.get());
+	@Test(description = "[TC_006] - Verify user is able to login with phone number", groups = { "regression",
+			"registration_and_login" })
+	public void TC_006_verifyUserLoginWithOTP() throws Exception {
+
+		try {
+			BaseTest.LOGGER.get().logTestStep(extentTest.get(), "INFO",
+					"<b><----Test case execution started for Test: TC_006 - Verify user is able to login with phone number ------><b>",
+					false, BaseTest.mobileDriver.get());
+
+			LoginKooPage loginKooPage = new LoginKooPage();
+			loginKooPage.loginValidUser("English", TestConfig.getInstance().getUserName());
+			BaseTest.LOGGER.get().logTestStep(extentTest.get(), "INFO",
+					"<b><----Test case execution completed for Test:Successfully executed - TC_006 - Verify user is able to login with phone number ------><b>",
+					true, BaseTest.mobileDriver.get());
+			BaseTest.afterMethodDriver.set(BaseTest.mobileDriver.get());
+		} catch (Exception e) {
+			BaseTest.utilObj.get().getAssertManager().sAssertException(
+					"Something went wrong in verifyUserLoginWithOTP. Exception:" + e.getMessage(), true,
+					BaseTest.mobileDriver.get());
 			BaseTest.afterMethodDriver.set(BaseTest.mobileDriver.get());
 		}
-    }
+	}
 }
